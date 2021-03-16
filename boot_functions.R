f517e9a78227b9d9171295f67a71b7c4f763c58b
Fcgrolleau <fc.grolleau@gmail.com>
2021-03-16 23:25:39 +0100

    AREa1r0 and AREa0r0 estimation

--- boot_functions.R
+++ boot_functions.R
@@ -2,3 +2,32 @@ are_boot <- function(d, i=1:nrow(d)) {
         z<-d[i,]
         return(with(z, mean(Y*(r-E)*(A-E)/(E*(1-E))) ))
 }
+
+y1a0r0_boot <- function(d, i=1:nrow(d)) {
+        z<-d[i,]
+        return(with(z, 
+                    sum(Y*A*r*(1-E)/E) / sum( (1-A)*r )) )
+}
+
+area0r0_boot <- function(d, i=1:nrow(d)) {
+        z<-d[i,]
+        return(with(z, sum(Y*A*r*(1-E)/E) / sum( (1-A)*r )) -
+                       with(z[z$A==0 & z$r!=z$A,], mean(Y)) )
+}
+
+y1a1r0_boot <- function(d, i=1:nrow(d)) {
+        z<-d[i,]
+        return(with(z, 
+                    sum(Y*(1-A)*E*(1-r)/(1-E)) / sum( (1-r)*A )
+        ))
+}
+
+area1r0_boot <- function(d, i=1:nrow(d)) {
+        z<-d[i,]
+        return(with(z, 
+                    sum(Y*(1-A)*E*(1-r)/(1-E)) / sum( (1-r)*A )) -
+                       with(z[z$A==1 & z$r!=z$A,], mean(Y))
+        )
+}
+
+
